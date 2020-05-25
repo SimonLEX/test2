3c01bed3acc8cdbbd130960e5bce23a24823eb84
SimonLEX <rudrjq417@naver.com>
2020-05-25 18:50:13 +0900

    Initial commit

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '''<!DOCTYPE HTML><html>
-  <head>
-    <title>Flask app</title>
-  </head>
-  <body>
-    <h1>Hello Flask!</h1>
-  </body>
-</html>'''
+    return render_template('index.html')
 
 
 if __name__=="__main__":
